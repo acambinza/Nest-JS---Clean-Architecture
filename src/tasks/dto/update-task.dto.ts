eb96ffe789e3ffb2407db9db86024ddca1e897f5
acambinza <acambinza@gmail.com>
2024-07-16 09:44:18 +0100

    feat: update items

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,16 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaskDto } from './create-task.dto';
 import { TaskStatus } from '../entities/task.entity';
 
 class _UpdateTaskDto {
   name: string;
-
   description?: string;
-
   start_at?: Date | null;
-
   end_at?: Date | null;
-
   status?: TaskStatus;
 }
 
